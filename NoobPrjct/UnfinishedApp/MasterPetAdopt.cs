66f9a02723429f422a8050ba91393e3fce2f2d76
Agung Fauzi <anggaaf007@justbond.onmicrosoft.com>
2024-09-25 00:19:59 +0700

    refactoring namespace

--- NoobPrjct/UnfinishedApp/MasterPetAdopt.cs
+++ NoobPrjct/UnfinishedApp/MasterPetAdopt.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NoobPrjct.UnsureApp
+namespace NoobPrjct.UnfinishedApp
 {
     public class MasterPetAdopt
     {
